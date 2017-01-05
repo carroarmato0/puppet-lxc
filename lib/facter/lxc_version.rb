1215278ba3b2fdddaa1b0301238aeaaabad68cb0
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-05 02:12:39 +0100

    Improve lxc version detection
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- lib/facter/lxc_version.rb
+++ lib/facter/lxc_version.rb
@@ -1,12 +1,10 @@
 Facter.add(:lxc_version) do
   confine :kernel => :Linux
   setcode do
-    version = Facter::Util::Resolution.exec('lxc --version')
-    if version
-      version.match(/\d+\.\d+\.\d+/).to_s
-    else
-      version = Facter::Util::Resolution.exec('lxc-console --version')
-      version.match(/\d+\.\d+\.\d+/).to_s
+    if (Facter::Util::Resolution.which('lxc') || Facter::Util::Resolution.which('lxc-console'))
+      lxc_version_command = Facter::Util::Resolution.which('lxc') ? 'lxc --version 2>&1' : 'lxc-console --version 2>&1'
+      lxc_version = Facter::Util::Resolution.exec(lxc_version_command)
+      lxc_version.match(/\d+\.\d+\.\d+/).to_s
     end
   end
 end
